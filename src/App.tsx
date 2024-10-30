1e72a5c1f2bbf12e052ccd8a49dfa9975cc9b933
Haneul <skyhn0207@naver.com>
2024-10-30 22:23:56 +0900

    feat: Add Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./pages/Root";
+import MainPage from "./pages/Main";
+import LoginPage from "./pages/Login";
 
 const router = createBrowserRouter([
-  { path: "/", element: <RootLayout />, id: "root", children: [] },
+  {
+    path: "/",
+    element: <RootLayout />,
+    id: "root",
+    children: [
+      { index: true, element: <MainPage /> },
+      { path: "login", element: <LoginPage /> },
+    ],
+  },
 ]);
 
 function App() {
