cea99d71bf14104f1c15507516fd0d6fa6e9501d
Haneul <skyhn0207@naver.com>
2024-11-03 21:13:29 +0900

    style: body, main min-height 100vh

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
   return (
-    <main className="px-5">
+    <main className="px-5 min-h-screen">
       <Outlet />
     </main>
   );
