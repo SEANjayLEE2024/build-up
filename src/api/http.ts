db9fae012e1c1919c3a25051ef13604f8d581a68
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2024-11-06 20:37:56 +0900

    feat: axios instance 생성 (임의의 백엔드 서버 설정값 입력)

---
+++ src/api/http.ts
@@ -0,0 +1,16 @@
+import axios, { AxiosRequestConfig } from "axios";
+
+const BASE_URL = "http://localhost:5005";
+
+const createClient = (config?: AxiosRequestConfig) => {
+  const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    withCredentials: true,
+    ...config,
+  });
+  return axiosInstance;
+};
+export const httpClient = createClient();
