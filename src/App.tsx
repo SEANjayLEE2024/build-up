726b712cc5aa758542d3b92ec200b08c06683a77
Haneul <skyhn0207@naver.com>
2024-11-07 22:43:08 +0900

    feat: Login 페이지 제작

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import RootLayout from "./pages/Root";
 import MainPage from "./pages/Main";
 import SignupPage from "./pages/Signup";
 import MyClubPage from "./pages/Myclub";
+import LoginPage from "./pages/Login";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,7 @@ const router = createBrowserRouter([
     id: "root",
     children: [
       { index: true, element: <MainPage /> },
+      { path: "login", element: <LoginPage /> },
       { path: "signup", element: <SignupPage /> },
       { path: "myclub", element: <MyClubPage /> },
     ],
---
+++ src/pages/Login.tsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+  return (
+    <>
+      <h5>로그인페이지</h5>
+    </>
+  );
+};
+
+export default LoginPage;
