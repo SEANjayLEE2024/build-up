2c6ba58efd53b3da3a0ab44188f3bc3f20c4e34a
Haneul <skyhn0207@naver.com>
2024-11-10 14:23:22 +0900

    feat: SetProfile 닉네임 휴효성 검사 추가

--- src/components/signup/SetProfile.tsx
+++ src/components/signup/SetProfile.tsx
@@ -1,19 +1,70 @@
 import { useState } from "react";
+import { UserProfile } from "../../models/signup.model";
 import Title from "../common/Title";
 import informationImg from "../../assets/images/information-line.png";
 
-const MAXLENGTH = 20;
+const NICKNAME_MAX_LENGTH = 20;
+const NICKNAME_ALLOW_PATTERN = /^[ㄱ-ㅎ가-힣a-zA-Z0-9]+$/;
 
 const SetProfile = () => {
-  const [nickname, setNickname] = useState<string>("");
+  const [profile, setProfile] = useState<UserProfile>({
+    nickname: "",
+    gender: null,
+    nation: "",
+    location: "",
+    age: "",
+  });
+  const [nicknameErrorText, setNicknameErrorText] = useState<string | null>(
+    null
+  );
 
   const onsubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
   };
 
-  const onChangeNickname = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNickname(event.target.value);
+  const onKeyDownNickname = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (
+      !NICKNAME_ALLOW_PATTERN.test(event.key) &&
+      event.key !== "Backspace" &&
+      event.key !== "Enter"
+    ) {
+      event.preventDefault();
+    }
   };
+
+  const onChangeNickname = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    setProfile((prev) => ({ ...prev, nickname: value }));
+
+    if (value === "") {
+      setNicknameErrorText(null);
+      return;
+    }
+
+    if (
+      value.length < 4 ||
+      value.length > 20 ||
+      !NICKNAME_ALLOW_PATTERN.test(value)
+    ) {
+      setNicknameErrorText(
+        "4 ~ 20자 이내의 한글, 영어, 숫자만 사용가능합니다."
+      );
+    } else {
+      setNicknameErrorText(null);
+    }
+  };
+
+  const onBlurNickname = (event: React.FocusEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    const koreanPattern = /[ㄱ-ㅎㅏ-ㅣ]/;
+
+    if (koreanPattern.test(value)) {
+      setNicknameErrorText("자음과 모음을 따로 사용할 수 없습니다.");
+      return;
+    }
+  };
+
   return (
     <>
       <form onSubmit={onsubmit}>
@@ -27,20 +78,29 @@ const SetProfile = () => {
             <input
               className="flex-1 min-w-12 outline-none"
               type="text"
-              maxLength={MAXLENGTH}
+              maxLength={NICKNAME_MAX_LENGTH}
               placeholder="닉네임을 입력해주세요."
               onChange={onChangeNickname}
+              onKeyDown={onKeyDownNickname}
+              onBlur={onBlurNickname}
             />
             <p className="text-gray">
-              <span>{nickname.length}</span>/<span>{MAXLENGTH}</span>
+              <span>{profile.nickname.length}</span>/
+              <span>{NICKNAME_MAX_LENGTH}</span>
             </p>
           </div>
 
-          <p className="flex text-[0.8125rem] gap-1 items-center mt-2 text-gray">
+          <p
+            className={`flex text-[0.8125rem] gap-1 items-center mt-2 text-gray ${
+              nicknameErrorText ? "text-red-600" : ""
+            }`}
+          >
             <i>
               <img src={informationImg} alt="" />
             </i>
-            특수문자 제외 영어, 한글, 숫자만 사용가능합니다.
+            {!nicknameErrorText
+              ? "특수문자 제외 영어, 한글, 숫자만 사용가능합니다."
+              : nicknameErrorText}
           </p>
         </section>
       </form>
---
+++ src/models/signup.model.ts
@@ -0,0 +1,7 @@
+export interface UserProfile {
+  nickname: string;
+  gender: number | null;
+  nation: string;
+  location: string;
+  age: string;
+}
