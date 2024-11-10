69d1a057faeff6129519ec650d9e3862cc7ed370
Haneul <skyhn0207@naver.com>
2024-11-10 21:06:21 +0900

    chore: index파일에 modal요소 추가, tailwind config 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,11 @@ export default {
         "base-secondary": "rgba(15, 19, 36, 0.6)",
         "base-secondary-hover": "#F7F7F8",
         "base-inverted": "#0B0C0E",
+        "base-divider": "#E9EAEC",
+        "alpha-black-40": "#00000066",
         "action-normal": "#DEE0E3",
         "focus-destructive-light": "#F08B85",
+        "overlay-active-normal": "#0A0F290A",
       },
       boxShadow: {
         "base-light": "0px 1px 2px 0px rgba(20, 21, 26, 0.05)",
