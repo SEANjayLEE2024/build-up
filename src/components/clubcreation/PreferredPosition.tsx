bcfd4bd0c894afca1e27b8b91ac4875b1f58685e
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2024-11-14 22:04:59 +0900

    feat: 선호 포메이션 input 개발
    (차후에 컴포넌트, 함수, 변수 분리 진행 해야 합니다)

--- src/components/clubcreation/PreferredPosition.tsx
+++ src/components/clubcreation/PreferredPosition.tsx
@@ -1,14 +1,165 @@
-import { ClubFormProps } from "../../models/clubcreation.model";
+import React, { useState } from "react";
+import { ClubCreationInputStateI } from "../../models/clubcreation.model";
 
-export default function PreferredPosition({ register }: ClubFormProps) {
+const futSalPosition = ["PIVO", "ALA", "FIXO", "GOLEIRO", "없음"];
+const soccerPosition = [
+  "ST",
+  "LW",
+  "RW",
+  "AM",
+  "MC",
+  "DM",
+  "LB",
+  "RB",
+  "CB",
+  "GK",
+  "없음",
+];
+
+interface Props {
+  clubCreationInputs: ClubCreationInputStateI;
+  setClubCreationInputs: React.Dispatch<
+    React.SetStateAction<ClubCreationInputStateI>
+  >;
+}
+
+export default function PreferredPosition({
+  clubCreationInputs,
+  setClubCreationInputs,
+}: Props) {
+  const AddPreferredFutsalPosition = (
+    e: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    const preferredPosition = e.currentTarget.value;
+    setClubCreationInputs((current) => ({
+      ...current,
+      clubRecruitment: {
+        ...current.clubRecruitment,
+        footsal: [
+          ...new Set([...current.clubRecruitment.footsal, preferredPosition]),
+        ],
+      },
+    }));
+  };
+
+  const AddPreferredSoccerPosition = (
+    e: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    const preferredPosition = e.currentTarget.value;
+    setClubCreationInputs((current) => ({
+      ...current,
+      clubRecruitment: {
+        ...current.clubRecruitment,
+        soccer: [
+          ...new Set([...current.clubRecruitment.soccer, preferredPosition]),
+        ],
+      },
+    }));
+  };
+
+  const DeletePreferredFutSalPosition = (
+    e: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    const notPreferredPosition = e.currentTarget.value;
+    const notPreferredPositionIndex =
+      clubCreationInputs.clubRecruitment.footsal.indexOf(notPreferredPosition);
+    const newFutsalPosition = [...clubCreationInputs.clubRecruitment.footsal];
+    newFutsalPosition.splice(notPreferredPositionIndex, 1);
+    setClubCreationInputs((current) => ({
+      ...current,
+      clubRecruitment: {
+        ...current.clubRecruitment,
+        footsal: newFutsalPosition,
+      },
+    }));
+  };
+
+  const DeletePreferredSoccerPosition = (
+    e: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    const notPreferredPosition = e.currentTarget.value;
+    const notPreferredPositionIndex =
+      clubCreationInputs.clubRecruitment.soccer.indexOf(notPreferredPosition);
+    const newSoccerPosition = [...clubCreationInputs.clubRecruitment.soccer];
+    newSoccerPosition.splice(notPreferredPositionIndex, 1);
+    setClubCreationInputs((current) => ({
+      ...current,
+      clubRecruitment: {
+        ...current.clubRecruitment,
+        soccer: newSoccerPosition,
+      },
+    }));
+  };
+
+  const checkFutsalPreferred = (position: string) => {
+    if (clubCreationInputs.clubRecruitment.footsal.includes(position)) {
+      return true;
+    }
+    return false;
+  };
+
+  const checkSoccerPreferred = (position: string) => {
+    if (clubCreationInputs.clubRecruitment.soccer.includes(position)) {
+      return true;
+    }
+    return false;
+  };
+
   return (
     <>
       <label className="mt-10">영입 희망 포지션</label>
-      <input
-        {...register("preferred_position", {
-          required: "필수 선택값입니다.",
-        })}
-      />
+      <div className="flex w-30 items-center">
+        <span>풋살 영입</span>
+        {futSalPosition.map((position, i) =>
+          checkFutsalPreferred(position) ? (
+            <button
+              value={position}
+              type="button"
+              className="p-2 cursor-pointer border-2 rounded border-slate-100 bg-lime-200"
+              key={i}
+              onClick={DeletePreferredFutSalPosition}
+            >
+              {position}
+            </button>
+          ) : (
+            <button
+              value={position}
+              type="button"
+              className="p-2 cursor-pointer border-2 rounded border-lime-200"
+              key={i}
+              onClick={AddPreferredFutsalPosition}
+            >
+              {position}
+            </button>
+          )
+        )}
+      </div>
+      <div className="flex w-30 items-center">
+        <span>축구 영입</span>
+        {soccerPosition.map((position, i) =>
+          checkSoccerPreferred(position) ? (
+            <button
+              value={position}
+              type="button"
+              className="p-2 cursor-pointer border-2 rounded border-slate-100 bg-lime-200"
+              key={i}
+              onClick={DeletePreferredSoccerPosition}
+            >
+              {position}
+            </button>
+          ) : (
+            <button
+              value={position}
+              type="button"
+              className="p-2 cursor-pointer border-2 rounded border-lime-200"
+              key={i}
+              onClick={AddPreferredSoccerPosition}
+            >
+              {position}
+            </button>
+          )
+        )}
+      </div>
     </>
   );
 }
